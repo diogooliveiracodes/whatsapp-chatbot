ecfb4a9630b3013c8b1f9c0d7e1baf601a8cde6d
Diogo Oliveira <diogo.o@icloud.com>
2025-03-12 17:59:11 -0300

    feat: create Unit Entity

---
+++ database/factories/UnitFactory.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Unit>
+ */
+class UnitFactory extends Factory
+{
+    const FIRST_COMPANY_ID = 1;
+    const ACTIVE = 1;
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'name' => $this->faker->unique()->city(),
+            'company_id' => self::FIRST_COMPANY_ID,
+            'active' => self::ACTIVE
+        ];
+    }
+}
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -20,6 +20,10 @@ class DatabaseSeeder extends Seeder
         );
 
         $this->call(
+            UnitSeeder::class
+        );
+
+        $this->call(
             UserRoleSeeder::class
         );
 
