36b7d15cfabd2cf98dd157da963dc3b399480141
Diogo Oliveira <diogo.o@icloud.com>
2025-03-18 17:49:53 -0300

    fix(Customer): creating Routes

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\CustomerController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
@@ -15,6 +16,16 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
+    Route::group(['prefix' => 'customers'], function () {
+        Route::get('/', [CustomerController::class, 'index'])->name('customers.index');
+        Route::get('/{customer}', [CustomerController::class, 'show'])->name('customers.show');
+        Route::get('/create', [CustomerController::class, 'create'])->name('customers.create');
+        Route::post('/', [CustomerController::class, 'store'])->name('customers.store');
+        Route::get('/{customer}/edit', [CustomerController::class, 'edit'])->name('customers.edit');
+        Route::put('/{customer}', [CustomerController::class, 'update'])->name('customers.update');
+        Route::delete('/{customer}', [CustomerController::class, 'destroy'])->name('customers.destroy');
+    });
 });
 
 require __DIR__.'/auth.php';
