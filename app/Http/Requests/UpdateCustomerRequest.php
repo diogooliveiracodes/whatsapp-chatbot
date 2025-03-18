e63186a719d3936161612c92954a2cde8cbf4a79
Diogo Oliveira <diogo.o@icloud.com>
2025-03-18 17:50:28 -0300

    fix(Customer): add UpdateCustomerRequest rules

--- app/Http/Requests/UpdateCustomerRequest.php
+++ app/Http/Requests/UpdateCustomerRequest.php
@@ -11,7 +11,7 @@ class UpdateCustomerRequest extends FormRequest
      */
     public function authorize(): bool
     {
-        return false;
+        return true;
     }
 
     /**
@@ -22,7 +22,19 @@ class UpdateCustomerRequest extends FormRequest
     public function rules(): array
     {
         return [
-            //
+            'active' => 'boolean',
+            'type' => 'required|in:1,2', // 1 - Individual, 2 - Company
+            'name' => 'required|string|max:120',
+            'document_number' => 'nullable|string|max:20',
+            'phone' => 'nullable|string|max:20',
+            'zip_code' => 'nullable|string|max:10',
+            'state' => 'nullable|string|max:2',
+            'city' => 'nullable|string|max:255',
+            'neighborhood' => 'nullable|string|max:255',
+            'street' => 'nullable|string|max:255',
+            'number' => 'nullable|string|max:20',
+            'complement' => 'nullable|string|max:255',
+            'prospect_origin' => 'nullable|string|max:255',
         ];
     }
 }
