28b474e0cc78acbb46324df134c1f0da3f5c54e2
Diogo Oliveira <diogo.o@icloud.com>
2025-03-25 09:58:19 -0300

    feat(ChatSession): add link to navbar

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -21,6 +21,11 @@
                         {{ __('Customers') }}
                     </x-nav-link>
                 </div>
+                <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
+                    <x-nav-link :href="route('chatSessions.index')" :active="request()->routeIs('chatSessions.*')">
+                        {{ __('Chat Sessions') }}
+                    </x-nav-link>
+                </div>
             </div>
 
             <!-- Settings Dropdown -->
