18390562e1cc0b562c3da158c83cca2d10a339cc
Diogo Oliveira <diogo.o@icloud.com>
2025-03-25 09:59:45 -0300

    feat(Message): add required fields to StoreMessageRequest.php

--- app/Http/Requests/StoreMessageRequest.php
+++ app/Http/Requests/StoreMessageRequest.php
@@ -11,7 +11,7 @@ class StoreMessageRequest extends FormRequest
      */
     public function authorize(): bool
     {
-        return false;
+        return true;
     }
 
     /**
@@ -22,7 +22,8 @@ class StoreMessageRequest extends FormRequest
     public function rules(): array
     {
         return [
-            //
+            'chat_session_id' => 'required|integer|exists:chat_sessions,id',
+            'content' => 'required|string',
         ];
     }
 }
