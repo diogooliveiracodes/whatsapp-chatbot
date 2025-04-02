f1804ae0dc3e566587ea71e5d7445fa4749913d1
Diogo Oliveira <diogo.o@icloud.com>
2025-04-02 15:41:32 -0300

    feat(UnitSettings): create controller and policy

---
+++ app/Http/Controllers/UnitSettingsController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\UnitSettings;
+use App\Http\Requests\StoreUnitSettingsRequest;
+use App\Http\Requests\UpdateUnitSettingsRequest;
+
+class UnitSettingsController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreUnitSettingsRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(UnitSettings $unitSettings)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(UnitSettings $unitSettings)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateUnitSettingsRequest $request, UnitSettings $unitSettings)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(UnitSettings $unitSettings)
+    {
+        //
+    }
+}
