f26bd90e7b8fc5f7ed2ec2580712e9554df47097
Diogo Oliveira <diogo.o@icloud.com>
2025-04-02 15:41:01 -0300

    feat(CompanySettings): create company_settings table

---
+++ database/migrations/2025_03_28_201651_create_company_settings_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('company_settings', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('company_id')->constrained('companies');
+            $table->string('name');
+            $table->string('identification')->nullable();
+            $table->string('phone')->nullable();
+            $table->string('whatsapp_webhook_url')->nullable();
+            $table->string('whatsapp_number')->nullable();
+            $table->string('default_language', 5)->nullable();
+            $table->string('timezone')->nullable();
+            $table->time('working_hour_start')->nullable();
+            $table->time('working_hour_end')->nullable();
+            $table->time('working_day_start')->nullable();
+            $table->time('working_day_end')->nullable();
+            $table->boolean('use_ai_chatbot')->default(false);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('company_settings');
+    }
+};
