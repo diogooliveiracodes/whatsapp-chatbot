6f8c496583eaf998935f113bd14b1395b2f3fe04
Diogo Oliveira <diogo.o@icloud.com>
2025-04-02 16:12:59 -0300

    feat(CompanySettings): create rules to StoreCompanySettingsRequest and UpdateCompanySettingsRequest

--- app/Http/Requests/UpdateCompanySettingsRequest.php
+++ app/Http/Requests/UpdateCompanySettingsRequest.php
@@ -3,6 +3,7 @@
 namespace App\Http\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rule;
 
 class UpdateCompanySettingsRequest extends FormRequest
 {
@@ -11,7 +12,7 @@ class UpdateCompanySettingsRequest extends FormRequest
      */
     public function authorize(): bool
     {
-        return false;
+        return true;
     }
 
     /**
@@ -22,7 +23,19 @@ class UpdateCompanySettingsRequest extends FormRequest
     public function rules(): array
     {
         return [
-            //
+            'company_id' => ['sometimes', 'exists:companies,id'],
+            'name' => ['sometimes', 'string', 'max:255'],
+            'identification' => ['nullable', 'string', 'max:255'],
+            'phone' => ['nullable', 'string', 'max:20'],
+            'whatsapp_webhook_url' => ['nullable', 'string', 'url', 'max:255'],
+            'whatsapp_number' => ['nullable', 'string', 'max:20'],
+            'default_language' => ['nullable', 'string', 'max:5'],
+            'timezone' => ['nullable', 'string', 'max:50'],
+            'working_hour_start' => ['nullable', 'date_format:H:i:s'],
+            'working_hour_end' => ['nullable', 'date_format:H:i:s'],
+            'working_day_start' => ['nullable', Rule::in([1, 2, 3, 4, 5, 6, 7])],
+            'working_day_end' => ['nullable', Rule::in([1, 2, 3, 4, 5, 6, 7])],
+            'use_ai_chatbot' => ['boolean'],
         ];
     }
 }
