f9b3cc883ca992f5cd8ad050ca377e0db747c38d
Diogo Oliveira <diogo.o@icloud.com>
2025-04-02 16:00:53 -0300

    fix(CompanySettings): create factory

--- database/factories/CompanySettingsFactory.php
+++ database/factories/CompanySettingsFactory.php
@@ -2,6 +2,7 @@
 
 namespace Database\Factories;
 
+use App\Models\Company;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 /**
@@ -9,6 +10,9 @@ use Illuminate\Database\Eloquent\Factories\Factory;
  */
 class CompanySettingsFactory extends Factory
 {
+    const MONDAY = 1;
+    const FRIDAY = 6;
+
     /**
      * Define the model's default state.
      *
@@ -17,7 +21,19 @@ class CompanySettingsFactory extends Factory
     public function definition(): array
     {
         return [
-            //
+            'company_id' => Company::factory(),
+            'name' => $this->faker->company,
+            'identification' => $this->faker->unique()->uuid,
+            'phone' => $this->faker->phoneNumber,
+            'whatsapp_webhook_url' => $this->faker->url,
+            'whatsapp_number' => $this->faker->phoneNumber,
+            'default_language' => $this->faker->randomElement(['en', 'pt', 'es', 'fr', 'de']),
+            'timezone' => $this->faker->timezone,
+            'working_hour_start' => '8:00:00',
+            'working_hour_end' => '18:00:00',
+            'working_day_start' => self::MONDAY,
+            'working_day_end' => self::FRIDAY,
+            'use_ai_chatbot' => $this->faker->boolean,
         ];
     }
 }
