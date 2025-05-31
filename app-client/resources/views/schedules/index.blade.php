9f55eb51c0e47c8a6ddcde03983d0482ce1299a0
Diogo Oliveira <diogo.oliveira@tray.net.br>
2025-05-31 16:23:07 -0300

    style(schedules): update header text color for improved visibility

--- app-client/resources/views/schedules/index.blade.php
+++ app-client/resources/views/schedules/index.blade.php
@@ -1,7 +1,7 @@
 <x-app-layout>
     <x-slot name="header">
         <div class="flex justify-between items-center">
-            <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            <h2 class="font-semibold text-xl text-gray-100 leading-tight">
                 {{ __('pages.schedules') }}
             </h2>
             <x-confirm-link :href="route('schedules.create')">
