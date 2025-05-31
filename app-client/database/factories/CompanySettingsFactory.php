f76d719869920fc5b0e68da421e4872c77307ad8
Diogo Oliveira <diogo.oliveira@tray.net.br>
2025-05-31 15:35:35 -0300

    refactor(factories): remove unused fields from CompanySettingsFactory and enhance UnitSettingsFactory with new fields

--- app-client/database/factories/CompanySettingsFactory.php
+++ app-client/database/factories/CompanySettingsFactory.php
@@ -10,9 +10,6 @@ use Illuminate\Database\Eloquent\Factories\Factory;
  */
 class CompanySettingsFactory extends Factory
 {
-    const MONDAY = 1;
-    const FRIDAY = 6;
-
     /**
      * Define the model's default state.
      *
@@ -22,18 +19,6 @@ class CompanySettingsFactory extends Factory
     {
         return [
             'company_id' => Company::factory(),
-            'name' => $this->faker->company,
-            'identification' => $this->faker->unique()->uuid,
-            'phone' => $this->faker->phoneNumber,
-            'whatsapp_webhook_url' => $this->faker->url,
-            'whatsapp_number' => $this->faker->phoneNumber,
-            'default_language' => $this->faker->randomElement(['en', 'pt', 'es', 'fr', 'de']),
-            'timezone' => $this->faker->timezone,
-            'working_hour_start' => '8:00:00',
-            'working_hour_end' => '18:00:00',
-            'working_day_start' => self::MONDAY,
-            'working_day_end' => self::FRIDAY,
-            'use_ai_chatbot' => $this->faker->boolean,
         ];
     }
 }
--- app-client/database/factories/UnitSettingsFactory.php
+++ app-client/database/factories/UnitSettingsFactory.php
@@ -9,6 +9,9 @@ use Illuminate\Database\Eloquent\Factories\Factory;
  */
 class UnitSettingsFactory extends Factory
 {
+    const MONDAY = 1;
+    const FRIDAY = 6;
+
     /**
      * Define the model's default state.
      *
@@ -17,7 +20,24 @@ class UnitSettingsFactory extends Factory
     public function definition(): array
     {
         return [
-            //
+            'company_id' => \App\Models\Company::factory(),
+            'unit_id' => \App\Models\Unit::factory(),
+            'name' => $this->faker->company,
+            'phone' => $this->faker->phoneNumber,
+            'street' => $this->faker->streetName,
+            'number' => $this->faker->buildingNumber,
+            'complement' => $this->faker->optional()->secondaryAddress,
+            'neighborhood' => $this->faker->citySuffix,
+            'city' => $this->faker->city,
+            'state' => $this->faker->stateAbbr,
+            'zipcode' => $this->faker->postcode,
+            'whatsapp_webhook_url' => $this->faker->url,
+            'whatsapp_number' => $this->faker->phoneNumber,
+            'working_hour_start' => '08:00:00',
+            'working_hour_end' => '18:00:00',
+            'working_day_start' => self::MONDAY,
+            'working_day_end' => self::FRIDAY,
+            'use_ai_chatbot' => $this->faker->boolean,
         ];
     }
 }
