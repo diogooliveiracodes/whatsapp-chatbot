facef4afced4805e8e9fac7a7b18f963cfef157c
Diogo Oliveira <diogo.oliveira@tray.net.br>
2025-05-31 15:31:56 -0300

    refactor(migrations): remove unused fields from company_settings migration and add relevant fields to unit_settings migration

--- app-client/database/migrations/2025_03_28_201651_create_company_settings_table.php
+++ app-client/database/migrations/2025_03_28_201651_create_company_settings_table.php
@@ -14,18 +14,6 @@ return new class extends Migration
         Schema::create('company_settings', function (Blueprint $table) {
             $table->id();
             $table->foreignId('company_id')->constrained('companies');
-            $table->string('name');
-            $table->string('identification')->nullable();
-            $table->string('phone')->nullable();
-            $table->string('whatsapp_webhook_url')->nullable();
-            $table->string('whatsapp_number')->nullable();
-            $table->string('default_language', 5)->nullable();
-            $table->string('timezone')->nullable();
-            $table->time('working_hour_start')->nullable();
-            $table->time('working_hour_end')->nullable();
-            $table->enum('working_day_start', [1, 2, 3, 4, 5, 6, 7])->nullable();
-            $table->enum('working_day_end', [1, 2, 3, 4, 5, 6, 7])->nullable();
-            $table->boolean('use_ai_chatbot')->default(false);
             $table->timestamps();
         });
     }
--- app-client/database/migrations/2025_03_28_201701_create_unit_settings_table.php
+++ app-client/database/migrations/2025_03_28_201701_create_unit_settings_table.php
@@ -4,8 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
@@ -24,6 +23,15 @@ return new class extends Migration
             $table->string('city')->nullable();
             $table->string('state')->nullable();
             $table->string('zipcode')->nullable();
+            $table->string('whatsapp_webhook_url')->nullable();
+            $table->string('whatsapp_number')->nullable();
+            $table->time('working_hour_start')->nullable();
+            $table->time('working_hour_end')->nullable();
+            $table->enum('working_day_start', [1, 2, 3, 4, 5, 6, 7])->nullable();
+            $table->enum('working_day_end', [1, 2, 3, 4, 5, 6, 7])->nullable();
+            $table->boolean('use_ai_chatbot')->default(false);
+            $table->string('default_language', 5)->nullable();
+            $table->string('timezone')->nullable();
             $table->timestamps();
         });
     }
