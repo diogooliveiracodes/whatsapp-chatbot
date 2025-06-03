39a8610a529983d8684058a6079d4bc0c12767b1
Diogo Oliveira <diogo.oliveira@tray.net.br>
2025-06-03 09:01:49 -0300

    refactor(header): remove user dropdown from header and move it to navigation layout for better structure

--- app-client/resources/views/components/header.blade.php
+++ app-client/resources/views/components/header.blade.php
@@ -1,22 +1,5 @@
-<div x-data="{ open: false }" class="flex items-center justify-between w-full p-4 bg-white dark:bg-gray-800 shadow-sm">
+<div class="flex items-center justify-between w-full p-4 bg-white dark:bg-gray-800 shadow-sm">
     <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
         {{ $slot }}
     </h2>
-    <div class="relative">
-        <button @click="open = !open" class="flex items-center px-4 py-2 text-left text-gray-500 dark:text-gray-400 hover:text-gray-700 dark:hover:text-gray-300 focus:outline-none">
-            <div>{{ Auth::user()->name }}</div>
-            <svg class="ml-auto h-4 w-4 fill-current" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
-                <path fill-rule="evenodd"
-                      d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
-                      clip-rule="evenodd"/>
-            </svg>
-        </button>
-        <div x-show="open" @click.away="open = false" class="absolute right-0 mt-2 w-48 bg-white dark:bg-gray-800 border border-gray-200 dark:border-gray-700 rounded-md shadow-lg">
-            <a href="{{ route('profile.edit') }}" class="block px-4 py-2 text-gray-700 dark:text-gray-200 hover:bg-gray-100 dark:hover:bg-gray-700">Profile</a>
-            <form method="POST" action="{{ route('logout') }}">
-                @csrf
-                <button type="submit" class="w-full text-left px-4 py-2 text-gray-700 dark:text-gray-200 hover:bg-gray-100 dark:hover:bg-gray-700">Log Out</button>
-            </form>
-        </div>
-    </div>
 </div>
--- app-client/resources/views/layouts/navigation.blade.php
+++ app-client/resources/views/layouts/navigation.blade.php
@@ -30,5 +30,24 @@
                 </x-nav-link>
             @endif
         </nav>
+
+        <!-- User Profile Dropdown -->
+        <div class="mt-auto">
+            <div x-data="{ open: false }" class="relative">
+                <button @click="open = !open" class="flex items-center w-full px-4 py-2 text-left text-gray-500 dark:text-gray-400 hover:text-gray-700 dark:hover:text-gray-300 focus:outline-none">
+                    <div>{{ Auth::user()->name }}</div>
+                    <svg class="ml-auto h-4 w-4 fill-current" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
+                        <path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd"></path>
+                    </svg>
+                </button>
+                <div x-show="open" @click.away="open = false" class="absolute left-0 bottom-full mb-2 w-48 bg-white dark:bg-gray-800 border border-gray-200 dark:border-gray-700 rounded-md shadow-lg" style="display: none;">
+                    <a href="{{ route('profile.edit') }}" class="block px-4 py-2 text-gray-700 dark:text-gray-200 hover:bg-gray-100 dark:hover:bg-gray-700">Profile</a>
+                    <form method="POST" action="{{ route('logout') }}">
+                        @csrf
+                        <button type="submit" class="w-full text-left px-4 py-2 text-gray-700 dark:text-gray-200 hover:bg-gray-100 dark:hover:bg-gray-700">Log Out</button>
+                    </form>
+                </div>
+            </div>
+        </div>
     </div>
 </div>
