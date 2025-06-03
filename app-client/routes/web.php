0928b9ec4c6b4274f0761bcb980d43c2411fda81
Diogo Oliveira <diogo.oliveira@tray.net.br>
2025-06-03 12:19:04 -0300

    refactor(routes): streamline unit management routes by removing redundant create route and cleaning up unit settings group

--- app-client/routes/web.php
+++ app-client/routes/web.php
@@ -38,25 +38,13 @@ Route::middleware('auth')->group(function () {
 
     Route::group(['prefix' => 'units'], function () {
         Route::get('/', [UnitController::class, 'index'])->name('units.index');
+        Route::get('/create', [UnitController::class, 'create'])->name('units.create');
         Route::get('/{unit}', [UnitController::class, 'show'])->name('units.show');
         Route::get('/{unit}/edit', [UnitController::class, 'edit'])->name('units.edit');
         Route::put('/{unit}', [UnitController::class, 'update'])->name('units.update');
         Route::delete('/{unit}', [UnitController::class, 'destroy'])->name('units.destroy');
-        Route::get('/create', [UnitController::class, 'create'])->name('units.create');
         Route::post('/', [UnitController::class, 'store'])->name('units.store');
     });
-
-    Route::group(
-        [
-            'prefix' => 'unit-settings',
-            'middleware' => ['auth', 'owner'],
-        ],
-        function () {
-            Route::get('/{unit_settings}', [UnitSettingsController::class, 'show'])->name('unit-settings.show');
-            Route::get('edit/{unit_settings}', [UnitSettingsController::class, 'edit'])->name('unit-settings.edit');
-            Route::put('/{unit_settings}', [UnitSettingsController::class, 'update'])->name('unit-settings.update');
-        },
-    );
 
     Route::get('/schedules', [ScheduleController::class, 'index'])->name('schedules.index');
     Route::get('/schedules/create', [ScheduleController::class, 'create'])->name('schedules.create');
