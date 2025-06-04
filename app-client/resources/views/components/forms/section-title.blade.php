c97874922ba0d4caad886fa61c887692acdcc934
Diogo Oliveira <diogo.oliveira@tray.net.br>
2025-06-04 19:43:27 -0300

    fix(sectionTitle): remove unnecessary margin from section title for improved layout consistency

--- app-client/resources/views/components/forms/section-title.blade.php
+++ app-client/resources/views/components/forms/section-title.blade.php
@@ -3,5 +3,5 @@
 ])
 
 <div class="col-span-2">
-    <h3 class="text-lg font-semibold text-gray-300 mb-4">{{ $title }}</h3>
+    <h3 class="text-lg font-semibold text-gray-300">{{ $title }}</h3>
 </div>
