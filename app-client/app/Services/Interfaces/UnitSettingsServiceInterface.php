cae9f1697ed81376e2743bc4a9c6661bdc35f278
Diogo Oliveira <diogo.oliveira@tray.net.br>
2025-06-04 11:47:55 -0300

    feat(unitSettings): introduce UnitSettingsServiceInterface for managing unit settings operations

---
+++ app-client/app/Services/Interfaces/UnitSettingsServiceInterface.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Services\Interfaces;
+
+use App\Models\UnitSettings;
+use Illuminate\Database\Eloquent\Collection;
+
+interface UnitSettingsServiceInterface
+{
+
+    /**
+     * Create new unit settings
+     *
+     * @param array $data
+     * @return UnitSettings
+     */
+    public function create(array $data): UnitSettings;
+
+    /**
+     * Update unit settings
+     *
+     * @param UnitSettings $unitSettings
+     * @param array $data
+     * @return UnitSettings
+     */
+    public function update(UnitSettings $unitSettings, array $data): UnitSettings;
+
+    /**
+     * Delete unit settings
+     *
+     * @param UnitSettings $unitSettings
+     * @return bool
+     */
+    public function delete(UnitSettings $unitSettings): bool;
+
+    /**
+     * Get unit settings by ID
+     *
+     * @param int $id
+     * @return UnitSettings|null
+     */
+    public function findById(int $id): ?UnitSettings;
+
+}
