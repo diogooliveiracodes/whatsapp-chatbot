40ed9ffb530880d119013cfd2b09158b2c5002d3
Diogo Oliveira <diogo.oliveira@tray.net.br>
2025-06-04 11:48:04 -0300

    feat(unitSettings): implement UnitSettingsService for CRUD operations on unit settings

---
+++ app-client/app/Services/UnitSettings/UnitSettingsService.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace App\Services\UnitSettings;
+
+use App\Repositories\UnitSettingsRepository;
+use Illuminate\Database\Eloquent\Collection;
+use App\Models\UnitSettings;
+use App\Services\Interfaces\UnitSettingsServiceInterface;
+
+class UnitSettingsService implements UnitSettingsServiceInterface
+{
+    public function __construct(protected UnitSettingsRepository $unitSettingsRepository) {}
+
+    /**
+     * Create new unit settings
+     *
+     * @param array $data
+     * @return UnitSettings
+     */
+    public function create(array $data): UnitSettings
+    {
+        return $this->unitSettingsRepository->create($data);
+    }
+
+    /**
+     * Update unit settings
+     *
+     * @param UnitSettings $unitSettings
+     * @param array $data
+     * @return UnitSettings
+     */
+    public function update(UnitSettings $unitSettings, array $data): UnitSettings
+    {
+        return $this->unitSettingsRepository->update($unitSettings, $data);
+    }
+
+    /**
+     * Delete unit settings
+     *
+     * @param UnitSettings $unitSettings
+     * @return bool
+     */
+    public function delete(UnitSettings $unitSettings): bool
+    {
+        return $this->unitSettingsRepository->delete($unitSettings);
+    }
+
+    /**
+     * Get unit settings by ID
+     *
+     * @param int $id
+     * @return UnitSettings|null
+     */
+    public function findById(int $id): ?UnitSettings
+    {
+        return $this->unitSettingsRepository->findById($id);
+    }
+
+}
