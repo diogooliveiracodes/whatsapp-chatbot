cc74917cc5bad7d29dc50c5222c6bebe5f1a3f5a
Diogo Oliveira <diogo.oliveira@tray.net.br>
2025-06-04 13:04:38 -0300

    refactor(schedule-calendar): simplify working days validation logic using an array of boolean flags

--- app-client/app/Services/Schedule/Validators/WorkingDaysValidator.php
+++ app-client/app/Services/Schedule/Validators/WorkingDaysValidator.php
@@ -4,12 +4,23 @@ namespace App\Services\Schedule\Validators;
 
 use App\Services\Schedule\Interfaces\WorkingDaysValidatorInterface;
 use Carbon\Carbon;
+use Illuminate\Support\Facades\Log;
 
 class WorkingDaysValidator implements WorkingDaysValidatorInterface
 {
     public function isOutsideWorkingDays(Carbon $date, $unitSettings): bool
     {
         $dayOfWeek = $date->dayOfWeek + 1;
-        return $dayOfWeek < $unitSettings->working_day_start || $dayOfWeek > $unitSettings->working_day_end;
+        $workingDays = [
+            1 => $unitSettings->sunday,
+            2 => $unitSettings->monday,
+            3 => $unitSettings->tuesday,
+            4 => $unitSettings->wednesday,
+            5 => $unitSettings->thursday,
+            6 => $unitSettings->friday,
+            7 => $unitSettings->saturday
+        ];
+
+        return !$workingDays[$dayOfWeek];
     }
 }
