728d79c46fd72edab88ccc9abe709a03a7d0a4cd
Diogo Oliveira <diogo.oliveira@tray.net.br>
2025-06-05 10:33:28 -0300

    feat(migrations): create migration for unit_service_types table with necessary fields and relationships

---
+++ app-client/database/migrations/2025_03_29_000001_create_unit_service_types_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('unit_service_types', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('company_id')->constrained()->onDelete('cascade');
+            $table->foreignId('unit_id')->constrained()->onDelete('cascade');
+            $table->string('name', 100);
+            $table->string('description', 255)->nullable();
+            $table->boolean('active')->default(true);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('unit_service_types');
+    }
+};
