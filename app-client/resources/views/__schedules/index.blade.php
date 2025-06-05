3a8d744fc054ae39b8a16ec7acdb8317f6885614
Diogo Oliveira <diogo.oliveira@tray.net.br>
2025-06-05 06:51:00 -0300

    refactor(Schedules): removing the use of external libraries

---
+++ app-client/resources/views/__schedules/index.blade.php
@@ -0,0 +1,15 @@
+<x-app-layout>
+    <x-slot name="header">
+        <div class="flex justify-between items-center">
+            <h2 class="font-semibold text-xl text-gray-100 leading-tight">
+                {{ __('pages.schedules') }}
+            </h2>
+            <x-confirm-link :href="route('schedules.create')">
+                {{ __('schedules.new_schedule') }}
+            </x-confirm-link>
+        </div>
+    </x-slot>
+
+    <x-schedule-calendar :schedules="$schedules" :unitSettings="$unit->unitSettings" />
+    <x-schedule-modal />
+</x-app-layout>
