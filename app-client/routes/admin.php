4746f0260655aced396e4ba7bba579632cad3fbf
Diogo Oliveira <diogo.oliveira@tray.net.br>
2025-06-26 18:15:40 -0300

    feat(admin-routes): implement user management routes for creating, viewing, editing, and deactivating users

--- app-client/routes/admin.php
+++ app-client/routes/admin.php
@@ -6,12 +6,11 @@ use App\Http\Controllers\AdminController;
 Route::middleware('admin')->group(function () {
     Route::get('/', [AdminController::class, 'index'])->name('admin.index');
     Route::group(['prefix' => 'users'], function () {
-
-    });
-    Route::group(['prefix' => 'companies'], function () {
-
-    });
-    Route::group(['prefix' => 'plans'], function () {
-
+        Route::get('/', [AdminController::class, 'users'])->name('admin.users');
+        Route::get('/create', [AdminController::class, 'createUser'])->name('admin.users.create');
+        Route::get('/{id}', [AdminController::class, 'showUser'])->name('admin.users.show');
+        Route::get('/{id}/edit', [AdminController::class, 'editUser'])->name('admin.users.edit');
+        Route::put('/{id}', [AdminController::class, 'updateUser'])->name('admin.users.update');
+        Route::patch('/{id}/deactivate', [AdminController::class, 'deactivateUser'])->name('admin.users.deactivate');
     });
 });
