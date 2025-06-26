8a8b96a79e55a08851736955497ee49b879393b6
Diogo Oliveira <diogo.oliveira@tray.net.br>
2025-06-26 19:48:42 -0300

    refactor(admin-routes): restructure admin routes to include 'admin' prefix and add user store route

--- app-client/routes/admin.php
+++ app-client/routes/admin.php
@@ -4,13 +4,16 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AdminController;
 
 Route::middleware('admin')->group(function () {
-    Route::get('/', [AdminController::class, 'index'])->name('admin.index');
-    Route::group(['prefix' => 'users'], function () {
-        Route::get('/', [AdminController::class, 'users'])->name('admin.users');
-        Route::get('/create', [AdminController::class, 'createUser'])->name('admin.users.create');
-        Route::get('/{id}', [AdminController::class, 'showUser'])->name('admin.users.show');
-        Route::get('/{id}/edit', [AdminController::class, 'editUser'])->name('admin.users.edit');
-        Route::put('/{id}', [AdminController::class, 'updateUser'])->name('admin.users.update');
-        Route::patch('/{id}/deactivate', [AdminController::class, 'deactivateUser'])->name('admin.users.deactivate');
+    Route::group(['prefix' => 'admin'], function () {
+        Route::get('/', [AdminController::class, 'index'])->name('admin.index');
+        Route::group(['prefix' => 'users'], function () {
+            Route::get('/', [AdminController::class, 'users'])->name('admin.users.index');
+            Route::get('/create', [AdminController::class, 'createUser'])->name('admin.users.create');
+            Route::post('/', [AdminController::class, 'storeUser'])->name('admin.users.store');
+            Route::get('/{id}', [AdminController::class, 'showUser'])->name('admin.users.show');
+            Route::get('/{id}/edit', [AdminController::class, 'editUser'])->name('admin.users.edit');
+            Route::put('/{id}', [AdminController::class, 'updateUser'])->name('admin.users.update');
+            Route::patch('/{id}/deactivate', [AdminController::class, 'deactivateUser'])->name('admin.users.deactivate');
+        });
     });
 });
