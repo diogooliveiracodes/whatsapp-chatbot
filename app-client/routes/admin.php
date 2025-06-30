f56e0f478e07c0a5b753b5f9a2714a741a221b7d
Diogo Oliveira <diogo.oliveira@tray.net.br>
2025-06-30 08:35:29 -0300

    feat(routes): add companies routes for admin panel, including index method

--- app-client/routes/admin.php
+++ app-client/routes/admin.php
@@ -19,5 +19,15 @@ Route::middleware('admin')->group(function () {
             Route::put('/{id}', [AdminController::class, 'updateUser'])->name('admin.users.update');
             Route::patch('/{id}/deactivate', [AdminController::class, 'deactivateUser'])->name('admin.users.deactivate');
         });
+
+        Route::group(['prefix' => 'companies'], function () {
+            Route::get('/', [AdminController::class, 'indexCompanies'])->name('admin.companies.index');
+            // Route::get('/create', [AdminController::class, 'createCompany'])->name('admin.companies.create');
+            // Route::post('/', [AdminController::class, 'storeCompany'])->name('admin.companies.store');
+            // Route::get('/{id}', [AdminController::class, 'showCompany'])->name('admin.companies.show');
+            // Route::get('/{id}/edit', [AdminController::class, 'editCompany'])->name('admin.companies.edit');
+            // Route::put('/{id}', [AdminController::class, 'updateCompany'])->name('admin.companies.update');
+            // Route::patch('/{id}/deactivate', [AdminController::class, 'deactivateCompany'])->name('admin.companies.deactivate');
+        });
     });
 });
