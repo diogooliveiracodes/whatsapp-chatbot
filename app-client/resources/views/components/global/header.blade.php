edcf2eef3705cf71902b085d4314e4e460c72bd6
Diogo Oliveira <diogo.o@icloud.com>
2025-08-12 16:41:54 -0300

    style(header): add padding and margin to header title for improved layout consistency

--- app-client/resources/views/components/global/header.blade.php
+++ app-client/resources/views/components/global/header.blade.php
@@ -1,5 +1,5 @@
 <div class="flex items-center justify-between w-full p-4 bg-white dark:bg-gray-800 shadow-sm">
-    <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
+    <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight pl-4 mx-8 lg:ml-0">
         {{ $slot }}
     </h2>
 </div>
