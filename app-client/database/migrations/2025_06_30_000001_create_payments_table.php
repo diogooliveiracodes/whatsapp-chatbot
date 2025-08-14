223eb3964837e72103b5eb9c5200e2efd3e7438b
Diogo Oliveira <diogo.o@icloud.com>
2025-08-14 13:49:28 -0300

    refactor(payments): improve foreign key constraints formatting in payments migration

--- app-client/database/migrations/2025_06_30_000001_create_payments_table.php
+++ app-client/database/migrations/2025_06_30_000001_create_payments_table.php
@@ -17,10 +17,17 @@ return new class extends Migration {
         Schema::create('payments', function (Blueprint $table) {
             $table->id();
             $table->foreignId('company_id')->constrained('companies');
-            $table->foreignId('schedule_id')->constrained('schedules')->nullable();
-            $table->foreignId('plan_id')->constrained('plans');
-            $table->foreignId('customer_id')->constrained('customers')->nullable();
-            $table->foreignId('user_id')->constrained('users')->nullable();
+            $table->foreignId('schedule_id')
+                ->nullable()
+                ->constrained('schedules');
+            $table->foreignId('plan_id')
+                ->constrained('plans');
+            $table->foreignId('customer_id')
+                ->nullable()
+                ->constrained('customers');
+            $table->foreignId('user_id')
+                ->nullable()
+                ->constrained('users');
             $table->enum('payment_method', PaymentMethodEnum::getValues());
             $table->enum('gateway', PaymentGatewayEnum::getValues());
             $table->enum('service', PaymentServiceEnum::getValues());
