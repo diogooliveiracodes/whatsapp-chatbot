ddc3c543e1672b1afc7533aafd89dce7afced33a
Diogo Oliveira <diogo.o@icloud.com>
2025-08-14 14:40:53 -0300

    refactor(asaas): remove getLabel method from AsaasCustomerTypeEnum for simplification

--- app-client/app/Enum/AsaasCustomerTypeEnum.php
+++ app-client/app/Enum/AsaasCustomerTypeEnum.php
@@ -12,8 +12,4 @@ enum AsaasCustomerTypeEnum: int
         return array_column(self::cases(), 'value');
     }
 
-    public static function getLabel(int $value): string
-    {
-        return self::cases()[$value]->name;
-    }
 }
