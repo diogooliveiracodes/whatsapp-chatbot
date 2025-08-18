ba5c6d5a439af008e1c1a9f86abbec36c57015e3
Diogo Oliveira <diogo.o@icloud.com>
2025-08-18 16:09:56 -0300

    feat(schedule): add current week and week view labels in English and Portuguese translations, update button text in weekly view

--- app-client/resources/views/schedules/weekly-desktop.blade.php
+++ app-client/resources/views/schedules/weekly-desktop.blade.php
@@ -20,7 +20,7 @@
                         </a>
                         <a href="{{ route('schedules.weekly', ['date' => now()->format('Y-m-d')]) }}"
                             class="inline-flex items-center px-4 py-2 bg-gray-800 dark:bg-gray-200 border border-transparent rounded-md font-semibold text-xs text-white dark:text-gray-800 uppercase tracking-widest hover:bg-gray-700 dark:hover:bg-white focus:bg-gray-700 dark:focus:bg-white active:bg-gray-900 dark:active:bg-gray-300 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 dark:focus:ring-offset-gray-800 transition ease-in-out duration-150">
-                            {{ __('schedules.today') }}
+                            {{ __('schedules.current_week') }}
                         </a>
                         <a href="{{ route('schedules.weekly', ['date' => $startOfWeek->copy()->addDays(7)->format('Y-m-d')]) }}"
                             class="inline-flex items-center px-4 py-2 bg-gray-800 dark:bg-gray-200 border border-transparent rounded-md font-semibold text-xs text-white dark:text-gray-800 uppercase tracking-widest hover:bg-gray-700 dark:hover:bg-white focus:bg-gray-700 dark:focus:bg-white active:bg-gray-900 dark:active:bg-gray-300 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 dark:focus:ring-offset-gray-800 transition ease-in-out duration-150">
