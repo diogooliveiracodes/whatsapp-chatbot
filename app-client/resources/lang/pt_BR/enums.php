02ff52ea4df41373df94b7e7508e29ed673db24c
Diogo Oliveira <diogo.o@icloud.com>
2025-08-26 15:23:33 -0300

    feat(customers): add translations for inactive customers and their management in English and Portuguese

---
+++ app-client/resources/lang/pt_BR/enums.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+    'customer_inactive_period' => [
+        'fifteen_days' => '15 dias',
+        'thirty_days' => '30 dias',
+        'forty_five_days' => '45 dias',
+        'sixty_days' => '60 dias',
+        'ninety_days' => '90 dias',
+    ],
+];
